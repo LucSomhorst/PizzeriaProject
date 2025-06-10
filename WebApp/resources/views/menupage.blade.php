4b83f3cf76d475d782a6e1f174f6b788fb8af355
LucSomhorst <ps265419@edu.summacollege.nl>
2025-06-10 16:02:19 +0200

    added store pages
    also added routes, and flowbite css plugin

---
+++ WebApp/resources/views/menupage.blade.php
@@ -0,0 +1,8 @@
+<head>
+    <script src="../path/to/flowbite/dist/flowbite.min.js"></script>
+
+</head>
+<div>
+    <!-- Be present above all else. - Naval Ravikant -->
+    text
+</div>
--- WebApp/routes/web.php
+++ WebApp/routes/web.php
@@ -5,3 +5,17 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::get('/cart', function () {
+    return view('cartpage');
+});
+Route::get('/menu', function () {
+    return view('menupage');
+});
+Route::get('/order', function () {
+    return view('orderpage');
+});
+
+Route::get('/checkout', function () {
+    return view('checkoutpage');
+});
